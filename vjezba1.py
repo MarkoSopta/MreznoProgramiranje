c3b2cad07023bf642dff04da667e8e3d48987cc4
MarkoSopta <philoyd99@gmail.com>
2023-12-05 11:52:29 +0100

    Updated Vjezba1

--- vjezba1.py
+++ vjezba1.py
@@ -2,49 +2,59 @@ import argparse
 import socket
 import sys
 
-def get_ip_address(hostname):
-    try:
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
-    except socket.gaierror:
-        sys.exit(f"Error: Hostname '{hostname}' not found.")
-
-def get_port_number(service_name, proto, order):
-    try:
-        port = socket.getservbyname(service_name, proto)
-        if order == 'n':
-            port = socket.htons(port)
-        return port
-    except (socket.error, OSError):
-        sys.exit(f"Error: Service '{service_name}' not found for protocol '{proto}'.")
-
-def print_result(ip_address, port, hex_format):
-
-    if hex_format:
-        port = hex(port)
-    print(f"IP Address: {ip_address}, Port: {port}")
-
 def main():
-    parser = argparse.ArgumentParser(description="Get IP address and port number of a service.")
-    parser.add_argument('-t', '--tcp', action='store_true', help="Use TCP (default)")
-    parser.add_argument('-u', '--udp', action='store_true', help="Use UDP")
-    parser.add_argument('-x', '--hex', action='store_true', help="Print port number in hex")
-    parser.add_argument('-n', '--network', action='store_true', help="Print port number in network byte order")
-    parser.add_argument('hostname', type=str, help="Hostname to resolve")
-    parser.add_argument('servicename', type=str, help="Service name to get port number for")
+    # Parsiranje argumenata
+    parser = argparse.ArgumentParser(description="Dohvati IP adresu, CNAME i broj porta usluge")
+    parser.add_argument("hostname", help="Ime računala")
+    parser.add_argument("servicename", help="Naziv usluge")
+    parser.add_argument("-u", "--udp", action="store_true", help="Koristi UDP protokol (TCP je pretpostavljen)")
+    parser.add_argument("-x", "--hex", action="store_true", help="Ispisi broj porta u heksadecimalnom obliku")
+    parser.add_argument("-n", "--network", action="store_true", help="Prikaži broj porta u network byte redoslijedu")
     args = parser.parse_args()
 
-    if args.tcp and args.udp:
-        sys.exit("Error: Options -t and -u cannot be used together.")
+    if len(sys.argv) == 1:
+        print("Uporaba:")
+        print("  getaddrinfo.py [-t|-u] [-x] [-h|-n] hostname servicename")
+        print("Opcije:")
+        print("  -t, --tcp         Koristi TCP protokol (TCP je pretpostavljen)")
+        print("  -u, --udp         Koristi UDP protokol")
+        print("  -x, --hex         Ispisi broj porta u heksadecimalnom obliku")
+        print("  -n, --network     Prikaži broj porta u network byte redoslijedu")
+        sys.exit()
 
+    # Dohvati informacije o servisu
+    try:
+        for info in socket.getaddrinfo(args.hostname, args.servicename, protocol=protocol):
+            address, _, _, _, port = info
+            break
+    except socket.gaierror as e:
+        print(f"Greška: {e}", file=sys.stderr)
+        sys.exit(1)
+     # Rukovanje greskama
+    if args.udp:
+        if args.tcp:
+            print("Greška: Mogu se koristiti samo jedna od opcija -t i -u", file=sys.stderr)
+            sys.exit(1)
+        if args.hex and args.network:
+            print("Greška: Mogu se koristiti samo jedna od opcija -x i -n", file=sys.stderr)
+            sys.exit(1)
+    if info is None:
+        print("Greška: Ne postoji adresa za navedene parametre", file=sys.stderr)
+        sys.exit(1)
 
-    proto = 'tcp' if args.tcp else 'udp'
-    order = 'h' if args.host else 'n'
+    # Transportni protokol
+    protocol = socket.AF_INET if not args.udp else socket.AF_INET6
 
-    ip_address = get_ip_address(args.hostname)
-    port = get_port_number(args.servicename, proto, order)
-    print_result(ip_address, port, args.hex)
+    # Ispisi IP adresu i CNAME
+    if address:
+        print(f"{address[0]} {socket.gethostbyaddr(address)[0]}", end=" ")
+
+    # Formatiranje porta
+    port_format = "x" if args.hex else "d"
+    byte_order = "!" if args.network else ""
+
+    # Ispisi broj porta
+    print(f"{port:{port_format}{byte_order}}")
 
 if __name__ == "__main__":
-
     main()
